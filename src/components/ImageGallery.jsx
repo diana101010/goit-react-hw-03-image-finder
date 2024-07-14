683df2daf5a2227eae5a57f8fc35d13755f7913d
diana101010 <150705147+diana101010@users.noreply.github.com>
2024-07-14 17:22:26 +0300

    com

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import ImageGalleryItem from './ImageGalleryItem';
 import style from './ImageGallery.module.css';
+
 const ImageGallery = ({ images }) => {
   return (
     <ul className={style.ImageGallery}>
       {images.map(image => (
-        <ImageGalleryItem key={image.id} image={image} />
+        <ImageGalleryItem key={image.id} image={image} src={image.API_URL} />
       ))}
     </ul>
   );
