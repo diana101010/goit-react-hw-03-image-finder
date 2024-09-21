2b78e27e27776de40bf3a938bff186addfea15a1
diana101010 <150705147+diana101010@users.noreply.github.com>
2024-09-21 15:38:10 +0300

    15

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Searchbar from './Searchbar';
+
 import ImageGallery from './ImageGallery';
 import Button from './Button';
 import Modal from './Modal';
